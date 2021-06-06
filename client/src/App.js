9f1312eb66c5c49d087e70aa55b2824ef97bac91
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2021-06-06 00:56:26 -0600

    restart

--- client/src/App.js
+++ client/src/App.js
@@ -1,94 +1,25 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
-import { toast } from 'react-toastify';
-import Dashboard from './components/Dashboard';
-import Login from './components/Login';
-import Register from './components/Register';
-import 'react-toastify/dist/ReactToastify.css';
+import logo from './logo.svg';
 import './App.css';
 
-toast.configure();
-
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const setAuth = (Boolean) => {
-    setIsAuthenticated(Boolean)
-  }
-
-  const isAuth = async() => {
-    try {
-      const response = await fetch("http://localhost:3003/auth/is-verify", {
-        method: "GET",
-        headers: { token: localStorage.token }
-      });
-
-      const parseRes = await response.json();
-      parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
-
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
-
-  useEffect(() => {
-    isAuth();
-  }, [])
-
   return (
-    <Fragment>
-      <Router>
-        <div className="container">
-          <Switch>
-            <Route 
-              exact
-              path="/"
-              render={props => 
-                !isAuthenticated ? (
-                  <Redirect to="/login" />
-                ) : (
-                  <Redirect to="/dashboard" />
-                )
-              }
-            />
-            <Route 
-              exact 
-              path="/login" 
-              render={props => 
-                !isAuthenticated ? ( 
-                  <Login {...props} setAuth={setAuth}/> 
-                ) : ( 
-                  <Redirect to="/dashboard" />
-                )
-              }
-            />
-            <Route 
-              exact 
-              path="/register" 
-              render={props => 
-                !isAuthenticated ? (
-                  <Register {...props} setAuth={setAuth}/> 
-                ) : (
-                  <Redirect to="/login" />
-                )
-              }
-            />
-            <Route 
-              exact 
-              path="/dashboard" 
-              render={props => 
-                isAuthenticated ? (
-                  <Dashboard {...props} setAuth={setAuth}/> 
-                ) : (
-                  <Redirect to="/login" />
-                )
-              }  
-            />
-          </Switch>
-        </div>
-      </Router>
-    </Fragment>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
